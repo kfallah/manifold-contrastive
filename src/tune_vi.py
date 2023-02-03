796831b0b4051c6f115c66119403fd585fa76b41
Kion <fallah.kion@gmail.com>
2023-02-03 13:48:08 -0500

    Update VI Tune Scripts

--- src/tune_vi.py
+++ src/tune_vi.py
@@ -1,4 +1,3 @@
-
 import logging
 import time
 import warnings
@@ -10,17 +9,19 @@ import omegaconf
 import torch
 import torch.distributions as distr
 import torch.nn as nn
-import wandb
 from hydra.core.config_store import ConfigStore
 from omegaconf import OmegaConf
 
+import wandb
 from dataloader.contrastive_dataloader import get_dataloader
 from model.config import ModelConfig
+from model.manifold.l1_inference import infer_coefficients
 from model.manifold.transop import TransOp_expm
 from model.model import Model
 from model.public.linear_warmup_cos_anneal import LinearWarmupCosineAnnealingLR
 
 warnings.filterwarnings("ignore")
+
 
 @dataclass
 class CoeffEncoderConfig:
@@ -32,7 +33,7 @@ class CoeffEncoderConfig:
     scale_prior: float = 0.02
     shift_prior: float = 0.0
     threshold: float = 0.032
-    
+
     logging_interval: int = 500
     lr: float = 0.03
     opt_type: str = "SGD"
@@ -58,17 +59,18 @@ class CoeffEncoderConfig:
     learn_prior: bool = False
     no_shift_prior: bool = False
 
+
 @dataclass
 class TransportOperatorConfig:
-    train_transop: bool = False
-    start_iter: int = 5000
+    train_transop: bool = True
+    start_iter: int = 1000
     dict_size: int = 100
 
     lr: float = 0.1
     weight_decay: float = 1.0e-5
 
     init_real_range: float = 2.0e-4
-    init_imag_range: float = 0.6
+    init_imag_range: float = 1.0
 
 
 @dataclass
@@ -82,8 +84,10 @@ class ExperimentConfig:
     run_dir: str = "../../results/Transop_sep-loss/"
     device: str = "cuda:0"
 
+    use_fista: bool = False
     vi_cfg: CoeffEncoderConfig = CoeffEncoderConfig()
     transop_cfg: TransportOperatorConfig = TransportOperatorConfig()
+
 
 class VIEncoder(nn.Module):
     def __init__(self, cfg: CoeffEncoderConfig, dict_size: int):
@@ -109,8 +113,8 @@ class VIEncoder(nn.Module):
                 nn.LeakyReLU(),
                 nn.Linear(cfg.hidden_dim2, cfg.feature_dim),
             )
-            self.scale_prior = nn.Linear(cfg.feature_dim, dict_size)  
-            self.shift_prior = nn.Linear(cfg.feature_dim, dict_size)       
+            self.scale_prior = nn.Linear(cfg.feature_dim, dict_size)
+            self.shift_prior = nn.Linear(cfg.feature_dim, dict_size)
 
     def soft_threshold(self, z: torch.Tensor, lambda_: torch.Tensor) -> torch.Tensor:
         return torch.nn.functional.relu(torch.abs(z) - lambda_) * torch.sign(z)
@@ -151,7 +155,9 @@ class VIEncoder(nn.Module):
                 c = self.reparameterize(noise, log_scale_expanded, shift_expanded)
                 T = torch.matrix_exp(torch.einsum("bsm,mpk->bspk", c, psi))
                 x1_hat = (T @ x0.unsqueeze(1).unsqueeze(-1)).squeeze(-1)
-                transop_loss = torch.nn.functional.mse_loss(x1_hat, x1.unsqueeze(1).repeat(1, self.cfg.samples_per_iter, 1), reduction="none").mean(dim=-1)
+                transop_loss = torch.nn.functional.mse_loss(
+                    x1_hat, x1.unsqueeze(1).repeat(1, self.cfg.samples_per_iter, 1), reduction="none"
+                ).mean(dim=-1)
 
                 noise_list.append(noise)
                 loss_list.append(transop_loss)
@@ -160,7 +166,7 @@ class VIEncoder(nn.Module):
             noise_list = torch.cat(noise_list, dim=1)
             loss_list = torch.cat(loss_list, dim=1)
             l1_list = torch.cat(l1_list, dim=1)
-            max_elbo = torch.argmin(loss_list + self.cfg.max_sample_l1_penalty*l1_list, dim=1).detach()
+            max_elbo = torch.argmin(loss_list + self.cfg.max_sample_l1_penalty * l1_list, dim=1).detach()
 
             # Pick out best noise sample for each batch entry for reparameterization
             optimal_noise = noise_list[torch.arange(len(x0)), max_elbo]
@@ -173,33 +179,34 @@ class VIEncoder(nn.Module):
             "distr/max_enc_scale": scale.max(),
             "distr/avg_enc_shift": shift.abs().mean(),
             "distr/min_enc_shift": shift.abs().min(),
-            "distr/max_enc_shift": shift.abs().max()
+            "distr/max_enc_shift": shift.abs().max(),
         }
         if self.cfg.learn_prior:
-            distr.update({
-                "distr/avg_prior_scale": prior_scale.mean(),
-                "distr/min_prior_scale": prior_scale.min(),
-                "distr/max_prior_scale": prior_scale.max(),
-                "distr/avg_prior_shift": prior_shift.abs().mean(),
-                "distr/min_prior_shift": prior_shift.abs().min(),
-                "distr/max_prior_shift": prior_shift.abs().max()
-            })
+            distr.update(
+                {
+                    "distr/avg_prior_scale": prior_scale.mean(),
+                    "distr/min_prior_scale": prior_scale.min(),
+                    "distr/max_prior_scale": prior_scale.max(),
+                    "distr/avg_prior_shift": prior_shift.abs().mean(),
+                    "distr/min_prior_shift": prior_shift.abs().min(),
+                    "distr/max_prior_shift": prior_shift.abs().max(),
+                }
+            )
 
         wandb.log(distr, step=curr_iter)
-
 
     def forward(self, curr_iter, x0, x1, psi):
         z = self.feat_extract(torch.cat((x0, x1), dim=-1))
         if not self.cfg.variational:
             shift = self.shift(z)
-            return shift, torch.tensor(0.), (torch.zeros_like(shift), shift)
+            return shift, torch.tensor(0.0), (torch.zeros_like(shift), shift)
 
         log_scale, shift = self.scale(z), self.shift(z)
         log_scale += torch.log(torch.ones_like(log_scale) * self.cfg.scale_prior)
         log_scale, shift = log_scale.clamp(min=-20, max=2), shift.clamp(min=-5, max=5)
 
         # Reparameterization
-        if (self.cfg.deterministic_pretrain and curr_iter < self.cfg.num_det_pretrain_iters):
+        if self.cfg.deterministic_pretrain and curr_iter < self.cfg.num_det_pretrain_iters:
             c = shift
         else:
             if self.cfg.enable_max_sample:
@@ -225,23 +232,28 @@ class VIEncoder(nn.Module):
 
         return c, kl, (log_scale, shift)
 
+
 def log_transop(psi, curr_iter):
-    psi_norm = (psi**2).sum(dim=(-1,-2))
+    psi_norm = (psi**2).sum(dim=(-1, -2))
     L = torch.linalg.eigvals(psi)
     eig_real = L.real
     eig_imag = L.imag
 
-    wandb.log({
-        "psi/avg_fnorm": psi_norm.mean(),
-        "psi/max_fnorm": psi_norm.max(),
-        "psi/min_fnorm": psi_norm.min(),
-        "psi/avg_real_eig": eig_real.abs().mean(),
-        "psi/max_real_eig": eig_real.abs().max(),
-        "psi/min_real_eig": eig_real.abs().min(),
-        "psi/avg_imag_eig": eig_imag.abs().mean(),
-        "psi/max_imag_eig": eig_imag.abs().max(),
-        "psi/min_imag_eig": eig_imag.abs().min(),
-    }, step=curr_iter)
+    wandb.log(
+        {
+            "psi/avg_fnorm": psi_norm.mean(),
+            "psi/max_fnorm": psi_norm.max(),
+            "psi/min_fnorm": psi_norm.min(),
+            "psi/avg_real_eig": eig_real.abs().mean(),
+            "psi/max_real_eig": eig_real.abs().max(),
+            "psi/min_real_eig": eig_real.abs().min(),
+            "psi/avg_imag_eig": eig_imag.abs().mean(),
+            "psi/max_imag_eig": eig_imag.abs().max(),
+            "psi/min_imag_eig": eig_imag.abs().min(),
+        },
+        step=curr_iter,
+    )
+
 
 def init_models(exp_cfg):
     current_checkpoint = 400
@@ -256,8 +268,10 @@ def init_models(exp_cfg):
     # Load model
     default_model_cfg = ModelConfig()
     model = Model.initialize_model(cfg.model_cfg, cfg.train_dataloader_cfg.dataset_cfg.dataset_name, device_idx)
-    state_dict = torch.load(exp_cfg.run_dir + f"checkpoints/checkpoint_epoch{current_checkpoint}.pt", map_location=default_device)
-    model.load_state_dict(state_dict['model_state'])
+    state_dict = torch.load(
+        exp_cfg.run_dir + f"checkpoints/checkpoint_epoch{current_checkpoint}.pt", map_location=default_device
+    )
+    model.load_state_dict(state_dict["model_state"])
     # Manually override directory for dataloaders
     cfg.train_dataloader_cfg.dataset_cfg.dataset_dir = "../../datasets"
     cfg.train_dataloader_cfg.batch_size = exp_cfg.vi_cfg.batch_size
@@ -266,45 +280,60 @@ def init_models(exp_cfg):
 
     # Initialize models
     if exp_cfg.transop_cfg.train_transop:
-        psi_init = TransOp_expm(exp_cfg.transop_cfg.dict_size, 64, stable_init=True, 
-                           real_range=exp_cfg.transop_cfg.init_real_range, 
-                           imag_range=exp_cfg.transop_cfg.init_imag_range).to(default_device)
+        psi_init = TransOp_expm(
+            exp_cfg.transop_cfg.dict_size,
+            64,
+            stable_init=True,
+            real_range=exp_cfg.transop_cfg.init_real_range,
+            imag_range=exp_cfg.transop_cfg.init_imag_range,
+        ).to(default_device)
         psi = psi_init.psi
     else:
         psi = model.contrastive_header.transop_header.transop.get_psi()
-    backbone = model.backbone
+    backbone = model.backbone.to(default_device)
     nn_bank = model.contrastive_header.transop_header.nn_memory_bank
-    encoder = VIEncoder(exp_cfg.vi_cfg, exp_cfg.transop_cfg.dict_size).to(default_device)
+    encoder = None
+    if not exp_cfg.use_fista:
+        encoder = VIEncoder(exp_cfg.vi_cfg, exp_cfg.transop_cfg.dict_size).to(default_device)
 
     return train_dataloader, backbone, nn_bank, psi, encoder
 
+
 def train(exp_cfg, train_dataloader, backbone, nn_bank, psi, encoder):
-    param_list = [
-        {
-            "params": encoder.parameters(),
-            "lr": exp_cfg.vi_cfg.lr,
-            "eta_min": 1e-3,
-            "weight_decay": exp_cfg.vi_cfg.weight_decay,
-            "disable_layer_adaptation": True,
-        },
-    ]
+    param_list = []
+    if encoder is not None:
+        param_list = [
+            {
+                "params": encoder.parameters(),
+                "lr": exp_cfg.vi_cfg.lr,
+                "eta_min": 1e-3,
+                "weight_decay": exp_cfg.vi_cfg.weight_decay,
+                "disable_layer_adaptation": True,
+            },
+        ]
     if exp_cfg.transop_cfg.train_transop:
-        param_list.append({
-            "params": psi,
-            "lr": exp_cfg.transop_cfg.lr,
-            "eta_min": 1e-3,
-            "weight_decay": exp_cfg.transop_cfg.weight_decay,
-            "disable_layer_adaptation": True,
-        })
+        param_list.append(
+            {
+                "params": psi,
+                "lr": exp_cfg.transop_cfg.lr,
+                "eta_min": 1e-3,
+                "weight_decay": exp_cfg.transop_cfg.weight_decay,
+                "disable_layer_adaptation": True,
+            }
+        )
     if exp_cfg.vi_cfg.opt_type == "SGD":
-        opt = torch.optim.SGD(param_list, lr=exp_cfg.vi_cfg.lr, nesterov=True, momentum=0.9, weight_decay=exp_cfg.vi_cfg.weight_decay)
+        opt = torch.optim.SGD(
+            param_list, lr=exp_cfg.vi_cfg.lr, nesterov=True, momentum=0.9, weight_decay=exp_cfg.vi_cfg.weight_decay
+        )
     elif exp_cfg.vi_cfg.opt_type == "AdamW":
         opt = torch.optim.AdamW(param_list, lr=exp_cfg.vi_cfg.lr, weight_decay=exp_cfg.vi_cfg.weight_decay)
     else:
         raise NotImplementedError()
     iters_per_epoch = len(train_dataloader)
-    scheduler = LinearWarmupCosineAnnealingLR(opt, warmup_epochs=20 * iters_per_epoch, max_epochs=iters_per_epoch*exp_cfg.num_epochs, eta_min=1e-4)
-    
+    scheduler = LinearWarmupCosineAnnealingLR(
+        opt, warmup_epochs=20 * iters_per_epoch, max_epochs=iters_per_epoch * exp_cfg.num_epochs, eta_min=1e-4
+    )
+
     transop_loss_save = []
     kl_loss_save = []
     dw_loss_save = []
@@ -312,10 +341,10 @@ def train(exp_cfg, train_dataloader, backbone, nn_bank, psi, encoder):
     iter_time = []
     for i in range(exp_cfg.num_epochs):
         for idx, batch in enumerate(train_dataloader):
-            curr_iter = i*len(train_dataloader) + idx
+            curr_iter = i * len(train_dataloader) + idx
             pre_time = time.time()
             x0, x1 = batch[0][0], batch[0][1]
-            x0, x1 = x0.cuda(), x1.cuda()
+            x0, x1 = x0.to(exp_cfg.device), x1.to(exp_cfg.device)
 
             with torch.no_grad():
                 z0, z1 = backbone(x0), backbone(x1)
@@ -324,7 +353,20 @@ def train(exp_cfg, train_dataloader, backbone, nn_bank, psi, encoder):
             z0 = torch.stack(torch.split(z0, 64, dim=-1)).transpose(0, 1).reshape(-1, 64)
             z1 = torch.stack(torch.split(z1, 64, dim=-1)).transpose(0, 1).reshape(-1, 64)
 
-            c, kl_loss, (log_scale, shift) = encoder(curr_iter, z0, z1, psi)
+            if exp_cfg.use_fista:
+                _, c = infer_coefficients(
+                    z0.float().detach(),
+                    z1.float().detach(),
+                    psi.data,
+                    exp_cfg.vi_cfg.threshold,
+                    max_iter=500,
+                    num_trials=1,
+                    device=z0.device,
+                )
+                kl_loss = torch.tensor(0.0)
+            else:
+                c, kl_loss, (log_scale, shift) = encoder(curr_iter, z0, z1, psi)
+
             if exp_cfg.transop_cfg.train_transop and curr_iter < exp_cfg.transop_cfg.start_iter:
                 psi_use = psi.detach()
             else:
@@ -333,22 +375,31 @@ def train(exp_cfg, train_dataloader, backbone, nn_bank, psi, encoder):
             z1_hat = (T @ z0.unsqueeze(-1)).squeeze(-1)
             transop_loss = torch.nn.functional.mse_loss(z1_hat, z1, reduction="none")
 
-            loss = transop_loss.mean() + exp_cfg.vi_cfg.kl_weight*kl_loss
+            loss = transop_loss.mean() + exp_cfg.vi_cfg.kl_weight * kl_loss
             if exp_cfg.vi_cfg.enable_c_l2:
                 l2_reg = (c**2).sum(dim=-1).mean()
                 loss += exp_cfg.vi_cfg.c_l2_weight * l2_reg
             if exp_cfg.vi_cfg.enable_shift_l2:
                 l2_reg = (shift**2).sum(dim=-1).mean()
-                loss += exp_cfg.vi_cfg.shift_l2_weight * l2_reg            
+                loss += exp_cfg.vi_cfg.shift_l2_weight * l2_reg
             (loss / exp_cfg.vi_cfg.grad_acc_iter).backward()
-            if curr_iter % exp_cfg.vi_cfg.logging_interval == 0:
+            if encoder is not None and curr_iter % exp_cfg.vi_cfg.logging_interval == 0:
                 enc_grad = torch.cat([param.grad.data.reshape(-1).detach().cpu() for param in encoder.parameters()])
-                wandb.log({
-                    'meta/avg_enc_grad': enc_grad.abs().mean(),
-                    'meta/med_enc_grad': torch.median(enc_grad.abs())
-                }, step=curr_iter)     
+                wandb.log(
+                    {
+                        "grad/avg_vi_grad": enc_grad.abs().mean(),
+                        "grad/max_vi_grad": enc_grad.abs().max(),
+                        "grad/med_vi_grad": torch.median(enc_grad.abs()),
+                        "grad/avg_to_grad": psi.grad.abs().mean() if psi.grad is not None else 0.0,
+                        "grad/max_to_grad": psi.grad.abs().max() if psi.grad is not None else 0.0,
+                        "grad/med_to_grad": torch.median(psi.grad.abs()) if psi.grad is not None else 0.0,
+                    },
+                    step=curr_iter,
+                )
 
-            if curr_iter % exp_cfg.vi_cfg.grad_acc_iter == 0:        
+            if curr_iter % exp_cfg.vi_cfg.grad_acc_iter == 0:
+                torch.nn.utils.clip_grad_norm_(psi, 0.1)
+                torch.nn.utils.clip_grad_norm_(encoder.parameters(), 0.1)
                 opt.step()
                 scheduler.step()
                 opt.zero_grad()
@@ -361,30 +412,37 @@ def train(exp_cfg, train_dataloader, backbone, nn_bank, psi, encoder):
             iter_time.append(time.time() - pre_time)
 
             if curr_iter % exp_cfg.vi_cfg.logging_interval == 0:
-                last_c = torch.cat(c_save[-exp_cfg.vi_cfg.logging_interval:])
+                last_c = torch.cat(c_save[-exp_cfg.vi_cfg.logging_interval :])
                 c_nz = torch.count_nonzero(last_c, dim=-1).float().mean()
                 c_mag = last_c[last_c.abs() > 0].abs().mean()
-                log.info(f"Iter {curr_iter:6n} -- TO loss: {np.mean(transop_loss_save[-exp_cfg.vi_cfg.logging_interval:]):.3E}," +
-                         f" KL loss: {np.mean(kl_loss_save[-exp_cfg.vi_cfg.logging_interval:]):.3E}," +
-                         f" dist improve: {np.mean(dw_loss_save[-exp_cfg.vi_cfg.logging_interval:]):.3E}," +
-                         f" c nonzero: {c_nz:.3f}," +
-                         f" c mag: {c_mag:.3f}," +
-                         f" time: {np.mean(iter_time[-exp_cfg.vi_cfg.logging_interval:]):.2f}sec")
+                log.info(
+                    f"Iter {curr_iter:6n} -- TO loss: {np.mean(transop_loss_save[-exp_cfg.vi_cfg.logging_interval:]):.3E},"
+                    + f" KL loss: {np.mean(kl_loss_save[-exp_cfg.vi_cfg.logging_interval:]):.3E},"
+                    + f" dist improve: {np.mean(dw_loss_save[-exp_cfg.vi_cfg.logging_interval:]):.3E},"
+                    + f" c nonzero: {c_nz:.3f},"
+                    + f" c mag: {c_mag:.3f},"
+                    + f" time: {np.mean(iter_time[-exp_cfg.vi_cfg.logging_interval:]):.2f}sec"
+                )
 
-                wandb.log({
-                    'loss/transop': np.mean(transop_loss_save[-exp_cfg.vi_cfg.logging_interval:]),
-                    'loss/kl': np.mean(kl_loss_save[-exp_cfg.vi_cfg.logging_interval:]),
-                    'loss/dist_improve': np.mean(dw_loss_save[-exp_cfg.vi_cfg.logging_interval:]),
-                    'meta/c_nonzero': c_nz,
-                    'meta/c_mag': c_mag,
-                    'meta/iter_time': np.mean(iter_time[-exp_cfg.vi_cfg.logging_interval:]),
-                }, step=curr_iter)
+                wandb.log(
+                    {
+                        "loss/transop": np.mean(transop_loss_save[-exp_cfg.vi_cfg.logging_interval :]),
+                        "loss/kl": np.mean(kl_loss_save[-exp_cfg.vi_cfg.logging_interval :]),
+                        "loss/dist_improve": np.mean(dw_loss_save[-exp_cfg.vi_cfg.logging_interval :]),
+                        "meta/c_nonzero": c_nz,
+                        "meta/c_mag": c_mag,
+                        "meta/iter_time": np.mean(iter_time[-exp_cfg.vi_cfg.logging_interval :]),
+                    },
+                    step=curr_iter,
+                )
 
                 if exp_cfg.transop_cfg.train_transop:
                     log_transop(psi.detach().cpu(), curr_iter)
 
+
 def register_configs() -> None:
     cs.store(name="exp_cfg", node=ExperimentConfig)
+
 
 @hydra.main(version_base=None, config_path="../config_vi", config_name="default_vi")
 def main(exp_cfg: ExperimentConfig) -> None:
@@ -404,6 +462,7 @@ def main(exp_cfg: ExperimentConfig) -> None:
     train_dataloader, backbone, nn_bank, psi, encoder = init_models(exp_cfg)
     log.info("Training Variational Encoder...")
     train(exp_cfg, train_dataloader, backbone, nn_bank, psi, encoder)
+
 
 if __name__ == "__main__":
     log = logging.getLogger(__name__)
